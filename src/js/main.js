87e3d00660a3b28494de665f0661a3d4b7f46f9c
merlingerin <lisg1990@gmail.com>
2017-09-06 23:15:36 +0300

    make a pagination and preloader

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,29 @@
 window.addEventListener('DOMContentLoaded', function(){
     $.init('.gallery__images');
     $.inputSeach();
+    $.currentSize('.filter__list');
 });
 
 //Init app
 var $ = (function(selector, context){
     var init = function(selector) {
-        return document.querySelector(selector + '');
-    }
-    var images = [],
-        filteredArr = [];
+        gallery = document.querySelector(selector + '');
+    };
 
-    (async function getImages() {
-        await fetch('https://unsplash.it/list')
+    var gallery = '',
+        images = [],
+        filteredArr = [],
+        currentPage = 0,
+        maxPage = 0;
+
+
+
+
+    ////////////////////////
+    //Fetch all photos from API
+    ///////////////////////
+    (function getImages() {
+              fetch('https://unsplash.it/list')
                     .then(function(response) {
                         return response.json();
                     })
@@ -26,6 +37,83 @@ var $ = (function(selector, context){
                     });
     }());
 
+    ////////////////////////////////
+    //Change current filter by size
+    ///////////////////////////////
+    function currentSize(elem) {
+        var btns = document.querySelector(elem);
+        btns.addEventListener('click', function(e) {
+          e.preventDefault();
+
+          var current = document.querySelector(elem + ' .active');
+          
+          current.classList.remove('active');
+          e.target.classList.add('active');
+
+        })     
+    }
+
+
+    ////////////////////////////////
+    //Render pagination
+    ///////////////////////////////
+    function pagination (currentPage, nextPage, maxPage) {
+      var prev = document.querySelector('.link--prev'),
+          next = document.querySelector('.link--next');
+
+      if( nextPage <= 0 ) {
+        prev.style.display = 'none';
+      }
+      else if (nextPage >= maxPage) {
+        next.style.display = 'none';
+      }
+      else {
+        prev.style.display = 'inline-block';
+        next.style.display = 'inline-block';
+      }
+
+      if(nextPage < 0) {
+        return 0;
+      }
+      else if(nextPage > maxPage) {
+        return maxPage;
+      }
+      else {
+        return nextPage;
+      }
+    }
+
+    ////////////////////////
+    //Render gallery
+    ///////////////////////
+    function renderGallery(images, perPage, idx ) {
+      console.log('currentPage gallery ', currentPage);
+
+      maxPage = Math.ceil(images.length/perPage);
+      var template = '',
+          imagesValue = images.length,
+          i = 0;
+
+
+
+      for ( i; perPage > i; i++) {
+
+        if(images[i + (currentPage * perPage)] == undefined ) {
+
+        }
+        else {
+            template += `<div class="image__container">
+                            <a href="#" class="image_link">
+                                <img src="https://unsplash.it/300/300?image=`+images[i + (currentPage * perPage)].id+`" alt="image">
+                                </a>
+                        </div>`;
+        }
+      }
+      gallery.innerHTML = template;
+    }
+    ////////////////////////
+    //Seach photos by Author
+    ///////////////////////
     function inputSeach() {
         var input = document.querySelector('.filter__input');
 
@@ -37,7 +125,10 @@ var $ = (function(selector, context){
                 console.log('Filtered by author: ', filteredArr);    
         });
     }
-        
+    
+    ////////////////////////
+    //Filter Photos by size
+    ///////////////////////    
     function filterPhotos(data, size) {
         var amount = amount,
             images = data,
@@ -47,16 +138,6 @@ var $ = (function(selector, context){
 
             filteredArr = images;
 
-            // if( !images ) {
-            //     template = `<div class="spinner">Loading...</div>`
-            // }
-            // for (var i = 0; amount > i; i++) {
-            //     template += `<div class="image__container">
-            //                         <a href="#" class="image_link">
-            //                             <img src="https://unsplash.it/300/300?image=`+images[i].id+`" alt="image">
-            //                             </a>
-            //                     </div>`;
-                // gallery__images.innerHTML(template);
         }
         else if (size === 'large') {
 
@@ -80,27 +161,47 @@ var $ = (function(selector, context){
 
         }
 
-        console.log('filteredArr: ', filteredArr);
-        document.querySelector('.gallery__images').innerHTML = template;
-        
+        renderGallery(filteredArr, 20);
+        console.log('filteredArr: ', filteredArr);        
     }
 
+    ////////////////////////
+    //Catch actions
+    ///////////////////////
     document.addEventListener("DOMContentLoaded", function(event) {
-        var btns = document.querySelectorAll('[data-action]');
+        var btns = document.querySelectorAll('[data-action]'),
+            pageNumber = document.querySelector('.page-number');
+
         btns.forEach(function(item, idx){
             btns[idx].addEventListener('click', function(e){
                 e.preventDefault();
                 var btn = e.target.dataset["action"];
                 switch(btn) {
                     case "getSmallPhoto":
+                        currentPage = pagination(currentPage, -maxPage, maxPage);
                         filterPhotos(images, 'small');
+                        pageNumber.innerHTML = ' [ '+ (currentPage + 1) +' ] ';
                         break;
                     case "getMiddlePhoto":
+                        currentPage = pagination(currentPage, -maxPage, maxPage);
                         filterPhotos(images, 'middle');
+                        pageNumber.innerHTML = ' [ '+ (currentPage + 1) +' ] ';
                         break;
                     case "getLargePhoto":
                         filterPhotos(images, 'large');
                         break;
+                    case "increasPage":
+                        currentPage = pagination(currentPage, currentPage + 1, maxPage);
+                        renderGallery(filteredArr, 20);
+                        pageNumber.innerHTML = ' [ '+ (currentPage + 1) +' ] ';
+                        break;
+                    case "decreasPage":
+                        currentPage = pagination(currentPage, currentPage - 1, maxPage);
+                        renderGallery(filteredArr, 20);
+                        pageNumber.innerHTML = ' [ '+ (currentPage + 1) +' ] ';
+                        break;
+                    default:
+                      return true;
                 }
             });
         })
@@ -121,6 +222,7 @@ var $ = (function(selector, context){
     return {
         inputSeach: inputSeach,
         init: init,
+        currentSize: currentSize
     }
 }());
 
